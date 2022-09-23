75483889c10cfc1cb2f16fe0399bf47552d81acb
lhenningson <l.henningson@share.epsb.ca>
2022-09-23 11:23:07 -0600

    io

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // JavaScript Code
+
+// Input
+let CS1 = +prompt(`Your Client-Side Scripting 1 grade:`);
+let SP1 = +prompt(`Your Structured Programming 1 grade:`); 
+let SP2 = +prompt(`Your Structured Programming 2 grade:`); 
+let CSS2 = +prompt(`Your Client-Side Scripting 2 grade:`); 
+let PA = +prompt(`Your Project A grade:`); 
+
+// Process
+let DIV = 5
+let Calc = CS1 + SP1 + SP2 + CSS2 + PA;
+let ANS = Calc / DIV;
+// Output
+alert(`Your Grade is ${ANS}.`);
